519b15edac79f5678686ffa5ac7a56a5cf2b85e7
rojefferson <rojefferson3@gmail.com>
2021-05-22 17:10:17 -0300

    primeira versão pdf

--- api.js
+++ api.js
@@ -11,7 +11,8 @@ const app = express();
 
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDlkOTg4YTk0MTM4ZjdiNWY4OTdmOTUiLCJsZXZlbCI6MSwiZHVlRGF0ZSI6IjIwMjEtMDYtMTNUMDM6MzI6NTIuNDE0KzAwOjAwIiwicGxhbiI6IjYwOWQ5ODU3OTQxMzhmN2I1Zjg5N2YyNSIsInVzZXJJcCI6IjAuMC4wLjAiLCJpYXQiOjE2MjA5NDEwNjZ9.IWO3hl-nwBzuMl7UtSF5xieCMH-X0jNkxYTxtfF9OkE'
 
-
+//TODO refatorar 
+//TODO colocar token 
 app.get('/teste', (req, res) => {
   axios.post('https://api.cyberlookup.org/api/v1/search/score3/cpf',{
         "cpf": "001.781.091-42"
@@ -22,16 +23,18 @@ app.get('/teste', (req, res) => {
       .then(res => {
         var site = HTMLParser.parse(res.data.consulta);
         var info = site.querySelectorAll("div[class='row resumo no-padding-left']")
-        // dividas
+        let dividas = [];
+        
         for (var i in info ){
           let prop = info[i].querySelector("strong").innerHTML.trim();
           let value =  info[i].querySelectorAll("div[class='pwl-col-xs-4 text-center']")[2].innerHTML.trim();
-          console.log(prop +  "-" + value);
+          dividas.push({nome:prop,valor:value});
         }
 
-        ejs.renderFile(path.join(__dirname, './views/', "template.ejs"), {students: students}, (err, data) => {
+        ejs.renderFile(path.join(__dirname, 'template.ejs'), {dividas: dividas}, (err, data) => {
           if (err) {
-                res.send(err);
+                console.log("🚀 ~ file: api.js ~ line 36 ~ ejs.renderFile ~ err", err)
+                //res.send(err);
           } else {
               let options = {
                   "height": "11.25in",
@@ -44,11 +47,12 @@ app.get('/teste', (req, res) => {
                   },
               };
               pdf.create(data, options).toFile("report.pdf", function (err, data) {
-                  if (err) {
-                      res.send(err);
-                  } else {
-                      res.send("File created successfully");
-                  }
+                  console.log("🚀 ~ file: api.js ~ line 49 ~ err", err)
+                  // if (err) {
+                  //     res.send(err);
+                  // } else {
+                  //     res.send("File created successfully");
+                  // }
               });
           }
       });
