5fbcba33b752181e79e56f7d5ffd83e6d9f1fdfa
rojefferson <rojefferson3@gmail.com>
2021-05-23 15:04:15 -0300

    template

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const  axios = require('axios');
-const HTMLParser = require('node-html-parser');
+// const HTMLParser = require('node-html-parser');
 
 const ejs = require("ejs");
 const pdf = require("html-pdf");
@@ -12,8 +12,9 @@ const app = express();
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDlkOTg4YTk0MTM4ZjdiNWY4OTdmOTUiLCJsZXZlbCI6MSwiZHVlRGF0ZSI6IjIwMjEtMDYtMTNUMDM6MzI6NTIuNDE0KzAwOjAwIiwicGxhbiI6IjYwOWQ5ODU3OTQxMzhmN2I1Zjg5N2YyNSIsInVzZXJJcCI6IjAuMC4wLjAiLCJpYXQiOjE2MjA5NDEwNjZ9.IWO3hl-nwBzuMl7UtSF5xieCMH-X0jNkxYTxtfF9OkE'
 
 
-function geraPDF(infoPessoais){
-      ejs.renderFile(path.join(__dirname, 'template.ejs'), {infoPessoais: infoPessoais}, (err, data) => {
+function geraPDF(infoPessoais,infoDividas){
+    //TODO fazer diretorio
+      ejs.renderFile(path.join(__dirname, 'template.ejs'), {infoPessoais: infoPessoais,infoDividas:infoDividas}, (err, data) => {
           if (err) {
                 console.log("🚀 ~ file: api.js ~ line 36 ~ ejs.renderFile ~ err", err)
                 //res.send(err);
@@ -22,7 +23,7 @@ function geraPDF(infoPessoais){
                   "height": "11.25in",
                   "width": "8.5in",
                   "header": {
-                      "height": "20mm"
+                    "height": "20mm"
                   },
                   "footer": {
                       "height": "20mm",
@@ -71,21 +72,34 @@ function RetornaDadosPessoa(infoPessoa){
  return dados;
 }
 
+var infoPessoais = {nome : "jefferson",CPF : "233.234.324-23",DataNascimento:"30/10/1994"}
+
+var infoDividas = [{informante : "MERCADO PAGO.COM REPRESENTACOES LTDA",contrato : "17977342",cidade : "SAO PAULO	SPS",valor:"5,24"}
+                   ,{informante : "MERCADO PAGO.COM REPRESENTACOES LTDA",contrato : "17977342",cidade : "SAO PAULO	SPS",valor:"10,24"}
+                   ,{informante : "MERCADO PAGO.COM REPRESENTACOES LTDA",contrato : "17977342",cidade : "SAO PAULO	SPS",valor:"15,24"}
+                  ]  
+                  
+
 app.get('/teste', (req, res) => {
-  axios.post('https://api.cyberlookup.org/api/v1/search/score3/cpf',{
-        "cpf": "001.781.091-42"
-       },{
-          headers: {
-            'Authorization': `Basic ${token}` 
-          }})
-      .then(res => {
-        var site = HTMLParser.parse(res.data.consulta);
-        let infoPessoa = site.querySelectorAll("div[class='pwl-col-xs-8']") 
-
-        let dadosPessoa = RetornaDadosPessoa(infoPessoa);
-
-        console.log(dadosPessoa);
-      })
+  geraPDF(infoPessoais,infoDividas);
+  res.send("foi");
 });
+
+// app.get('/teste', (req, res) => {
+//   axios.post('https://api.cyberlookup.org/api/v1/search/score3/cpf',{
+//         "cpf": "001.781.091-42"
+//        },{
+//           headers: {
+//             'Authorization': `Basic ${token}` 
+//           }})
+//       .then(res => {
+//         var site = HTMLParser.parse(res.data.consulta);
+//         let infoPessoa = site.querySelectorAll("div[class='pwl-col-xs-8']") 
+
+//         let dadosPessoa = RetornaDadosPessoa(infoPessoa);
+
+//         console.log(dadosPessoa);
+//       })
+// });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
